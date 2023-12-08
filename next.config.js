148359efd053406994826c2478074234a671a247
hsk-kr <hsk.coder@gmail.com>
2023-12-08 19:01:45 +0100

    fix: .env name typo

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export',
   assetPrefix: process.env.ASSET_PREFIX,
-  basePath: process.env.BASE_PATHH,
+  basePath: process.env.BASE_PATH,
 };
 
 module.exports = nextConfig;
